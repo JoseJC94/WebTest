11cc3a34677707997ee8c55c43c291362d2c705a
JoseJC94 <josejc.crc@gmail.com>
2021-10-04 09:41:21 -0600

    Minor fix to query.sql

--- scripts/query.sql
+++ scripts/query.sql
@@ -1,15 +1,15 @@
 CREATE DATABASE WebTest;
 
 CREATE TABLE Estudiantes (
-    id INT(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(30) NULL,
-    edad INT(3) NULL,
-    universidad INT(2) NOT NULL,
+                             id INT(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                             nombre VARCHAR(30) NULL,
+                             edad INT(3) NULL,
+                             universidad INT(2) NOT NULL
 );
 
 CREATE TABLE Universidades (
-  id INT(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(30) NOT NULL,
+                               id INT(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                               nombre VARCHAR(30) NOT NULL
 );
 
 INSERT INTO Estudiantes (nombre, edad, universidad) VALUES ('Pedro', 22, 'UNA');
